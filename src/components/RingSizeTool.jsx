24b60b5c5ae0c5177f8fb8af22d1655268e0da6b
Mausam100 <smausam104@gmail.com>
2025-01-09 15:32:20 +0530

    some

--- src/components/RingSizeTool.jsx
+++ src/components/RingSizeTool.jsx
@@ -78,7 +78,7 @@ const RingSizeTool = () => {
           onClick={checkDeviceDetails}
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
-          Check My Device Details
+          Start Measurement
         </button>
 
         {/* {isDetailsChecked && (
